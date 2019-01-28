0d0e35f5db8e80e9185b19c24e53aabef381eb67
Abhi Yerra <abhi@berkeley.edu>
2019-01-28 14:31:36 -0800

    Fix the action

--- workflow/app_workflow.proto3.pb.go
+++ workflow/app_workflow.proto3.pb.go
@@ -6,6 +6,7 @@ package workflow
 import proto "github.com/golang/protobuf/proto"
 import fmt "fmt"
 import math "math"
+import any "github.com/golang/protobuf/ptypes/any"
 
 import (
 	context "golang.org/x/net/context"
@@ -23,7 +24,7 @@ var _ = math.Inf
 // proto package needs to be updated.
 const _ = proto.ProtoPackageIsVersion2 // please upgrade the proto package
 
-type AppWorkflowBody struct {
+type AppRequest struct {
 	Fields               []*Field          `protobuf:"bytes,1,rep,name=fields,proto3" json:"fields,omitempty"`
 	Environment          map[string]string `protobuf:"bytes,2,rep,name=environment,proto3" json:"environment,omitempty" protobuf_key:"bytes,1,opt,name=key,proto3" protobuf_val:"bytes,2,opt,name=value,proto3"`
 	Action               string            `protobuf:"bytes,3,opt,name=action,proto3" json:"action,omitempty"`
@@ -32,54 +33,118 @@ type AppWorkflowBody struct {
 	XXX_sizecache        int32             `json:"-"`
 }
 
-func (m *AppWorkflowBody) Reset()         { *m = AppWorkflowBody{} }
-func (m *AppWorkflowBody) String() string { return proto.CompactTextString(m) }
-func (*AppWorkflowBody) ProtoMessage()    {}
-func (*AppWorkflowBody) Descriptor() ([]byte, []int) {
-	return fileDescriptor_app_workflow_d9d28599486e0e9d, []int{0}
+func (m *AppRequest) Reset()         { *m = AppRequest{} }
+func (m *AppRequest) String() string { return proto.CompactTextString(m) }
+func (*AppRequest) ProtoMessage()    {}
+func (*AppRequest) Descriptor() ([]byte, []int) {
+	return fileDescriptor_app_workflow_0afb54f19affbdf6, []int{0}
 }
-func (m *AppWorkflowBody) XXX_Unmarshal(b []byte) error {
-	return xxx_messageInfo_AppWorkflowBody.Unmarshal(m, b)
+func (m *AppRequest) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_AppRequest.Unmarshal(m, b)
 }
-func (m *AppWorkflowBody) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
-	return xxx_messageInfo_AppWorkflowBody.Marshal(b, m, deterministic)
+func (m *AppRequest) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_AppRequest.Marshal(b, m, deterministic)
 }
-func (dst *AppWorkflowBody) XXX_Merge(src proto.Message) {
-	xxx_messageInfo_AppWorkflowBody.Merge(dst, src)
+func (dst *AppRequest) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_AppRequest.Merge(dst, src)
 }
-func (m *AppWorkflowBody) XXX_Size() int {
-	return xxx_messageInfo_AppWorkflowBody.Size(m)
+func (m *AppRequest) XXX_Size() int {
+	return xxx_messageInfo_AppRequest.Size(m)
 }
-func (m *AppWorkflowBody) XXX_DiscardUnknown() {
-	xxx_messageInfo_AppWorkflowBody.DiscardUnknown(m)
+func (m *AppRequest) XXX_DiscardUnknown() {
+	xxx_messageInfo_AppRequest.DiscardUnknown(m)
 }
 
-var xxx_messageInfo_AppWorkflowBody proto.InternalMessageInfo
+var xxx_messageInfo_AppRequest proto.InternalMessageInfo
 
-func (m *AppWorkflowBody) GetFields() []*Field {
+func (m *AppRequest) GetFields() []*Field {
 	if m != nil {
 		return m.Fields
 	}
 	return nil
 }
 
-func (m *AppWorkflowBody) GetEnvironment() map[string]string {
+func (m *AppRequest) GetEnvironment() map[string]string {
 	if m != nil {
 		return m.Environment
 	}
 	return nil
 }
 
-func (m *AppWorkflowBody) GetAction() string {
+func (m *AppRequest) GetAction() string {
 	if m != nil {
 		return m.Action
 	}
 	return ""
 }
 
+type AppResponse struct {
+	Fields               []*Field          `protobuf:"bytes,1,rep,name=fields,proto3" json:"fields,omitempty"`
+	Environment          map[string]string `protobuf:"bytes,2,rep,name=environment,proto3" json:"environment,omitempty" protobuf_key:"bytes,1,opt,name=key,proto3" protobuf_val:"bytes,2,opt,name=value,proto3"`
+	Action               string            `protobuf:"bytes,3,opt,name=action,proto3" json:"action,omitempty"`
+	Raw                  *any.Any          `protobuf:"bytes,4,opt,name=raw,proto3" json:"raw,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}          `json:"-"`
+	XXX_unrecognized     []byte            `json:"-"`
+	XXX_sizecache        int32             `json:"-"`
+}
+
+func (m *AppResponse) Reset()         { *m = AppResponse{} }
+func (m *AppResponse) String() string { return proto.CompactTextString(m) }
+func (*AppResponse) ProtoMessage()    {}
+func (*AppResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_app_workflow_0afb54f19affbdf6, []int{1}
+}
+func (m *AppResponse) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_AppResponse.Unmarshal(m, b)
+}
+func (m *AppResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_AppResponse.Marshal(b, m, deterministic)
+}
+func (dst *AppResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_AppResponse.Merge(dst, src)
+}
+func (m *AppResponse) XXX_Size() int {
+	return xxx_messageInfo_AppResponse.Size(m)
+}
+func (m *AppResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_AppResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_AppResponse proto.InternalMessageInfo
+
+func (m *AppResponse) GetFields() []*Field {
+	if m != nil {
+		return m.Fields
+	}
+	return nil
+}
+
+func (m *AppResponse) GetEnvironment() map[string]string {
+	if m != nil {
+		return m.Environment
+	}
+	return nil
+}
+
+func (m *AppResponse) GetAction() string {
+	if m != nil {
+		return m.Action
+	}
+	return ""
+}
+
+func (m *AppResponse) GetRaw() *any.Any {
+	if m != nil {
+		return m.Raw
+	}
+	return nil
+}
+
 func init() {
-	proto.RegisterType((*AppWorkflowBody)(nil), "AppWorkflowBody")
-	proto.RegisterMapType((map[string]string)(nil), "AppWorkflowBody.EnvironmentEntry")
+	proto.RegisterType((*AppRequest)(nil), "AppRequest")
+	proto.RegisterMapType((map[string]string)(nil), "AppRequest.EnvironmentEntry")
+	proto.RegisterType((*AppResponse)(nil), "AppResponse")
+	proto.RegisterMapType((map[string]string)(nil), "AppResponse.EnvironmentEntry")
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -94,7 +159,7 @@ const _ = grpc.SupportPackageIsVersion4
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type AppWorkflowClient interface {
-	Invoke(ctx context.Context, in *AppWorkflowBody, opts ...grpc.CallOption) (*AppWorkflowBody, error)
+	Invoke(ctx context.Context, in *AppRequest, opts ...grpc.CallOption) (*AppResponse, error)
 }
 
 type appWorkflowClient struct {
@@ -105,8 +170,8 @@ func NewAppWorkflowClient(cc *grpc.ClientConn) AppWorkflowClient {
 	return &appWorkflowClient{cc}
 }
 
-func (c *appWorkflowClient) Invoke(ctx context.Context, in *AppWorkflowBody, opts ...grpc.CallOption) (*AppWorkflowBody, error) {
-	out := new(AppWorkflowBody)
+func (c *appWorkflowClient) Invoke(ctx context.Context, in *AppRequest, opts ...grpc.CallOption) (*AppResponse, error) {
+	out := new(AppResponse)
 	err := c.cc.Invoke(ctx, "/AppWorkflow/Invoke", in, out, opts...)
 	if err != nil {
 		return nil, err
@@ -116,7 +181,7 @@ func (c *appWorkflowClient) Invoke(ctx context.Context, in *AppWorkflowBody, opt
 
 // AppWorkflowServer is the server API for AppWorkflow service.
 type AppWorkflowServer interface {
-	Invoke(context.Context, *AppWorkflowBody) (*AppWorkflowBody, error)
+	Invoke(context.Context, *AppRequest) (*AppResponse, error)
 }
 
 func RegisterAppWorkflowServer(s *grpc.Server, srv AppWorkflowServer) {
@@ -124,7 +189,7 @@ func RegisterAppWorkflowServer(s *grpc.Server, srv AppWorkflowServer) {
 }
 
 func _AppWorkflow_Invoke_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(AppWorkflowBody)
+	in := new(AppRequest)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
@@ -136,7 +201,7 @@ func _AppWorkflow_Invoke_Handler(srv interface{}, ctx context.Context, dec func(
 		FullMethod: "/AppWorkflow/Invoke",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(AppWorkflowServer).Invoke(ctx, req.(*AppWorkflowBody))
+		return srv.(AppWorkflowServer).Invoke(ctx, req.(*AppRequest))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -154,22 +219,27 @@ var _AppWorkflow_serviceDesc = grpc.ServiceDesc{
 	Metadata: "app_workflow.proto3",
 }
 
-func init() { proto.RegisterFile("app_workflow.proto3", fileDescriptor_app_workflow_d9d28599486e0e9d) }
+func init() { proto.RegisterFile("app_workflow.proto3", fileDescriptor_app_workflow_0afb54f19affbdf6) }
 
-var fileDescriptor_app_workflow_d9d28599486e0e9d = []byte{
-	// 212 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0x4e, 0x2c, 0x28, 0x88,
-	0x2f, 0xcf, 0x2f, 0xca, 0x4e, 0xcb, 0xc9, 0x2f, 0xd7, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x37, 0x96,
-	0xe2, 0x4d, 0xcb, 0x4c, 0xcd, 0x49, 0x29, 0x86, 0x72, 0x95, 0xce, 0x31, 0x72, 0xf1, 0x3b, 0x16,
-	0x14, 0x84, 0x43, 0x55, 0x39, 0xe5, 0xa7, 0x54, 0x0a, 0xc9, 0x71, 0xb1, 0x41, 0x14, 0x49, 0x30,
-	0x2a, 0x30, 0x6b, 0x70, 0x1b, 0xb1, 0xe9, 0xb9, 0x81, 0xb8, 0x41, 0x50, 0x51, 0x21, 0x67, 0x2e,
-	0xee, 0xd4, 0xbc, 0xb2, 0xcc, 0xa2, 0xfc, 0xbc, 0xdc, 0xd4, 0xbc, 0x12, 0x09, 0x26, 0xb0, 0x22,
-	0x45, 0x3d, 0x34, 0x63, 0xf4, 0x5c, 0x11, 0x6a, 0x5c, 0xf3, 0x4a, 0x8a, 0x2a, 0x83, 0x90, 0x75,
-	0x09, 0x89, 0x71, 0xb1, 0x25, 0x26, 0x97, 0x64, 0xe6, 0xe7, 0x49, 0x30, 0x2b, 0x30, 0x6a, 0x70,
-	0x06, 0x41, 0x79, 0x52, 0x76, 0x5c, 0x02, 0xe8, 0x1a, 0x85, 0x04, 0xb8, 0x98, 0xb3, 0x53, 0x2b,
-	0x25, 0x18, 0xc1, 0x0a, 0x41, 0x4c, 0x21, 0x11, 0x2e, 0xd6, 0xb2, 0xc4, 0x9c, 0xd2, 0x54, 0x09,
-	0x26, 0xb0, 0x18, 0x84, 0x63, 0xc5, 0x64, 0xc1, 0x68, 0x64, 0xcb, 0xc5, 0x8d, 0xe4, 0x10, 0x21,
-	0x3d, 0x2e, 0x36, 0xcf, 0xbc, 0xb2, 0xfc, 0xec, 0x54, 0x21, 0x01, 0x74, 0x07, 0x4a, 0x61, 0x88,
-	0x28, 0x31, 0x24, 0xb1, 0x41, 0xc2, 0x05, 0x10, 0x00, 0x00, 0xff, 0xff, 0xb1, 0x4d, 0x76, 0x3d,
-	0x3c, 0x01, 0x00, 0x00,
+var fileDescriptor_app_workflow_0afb54f19affbdf6 = []byte{
+	// 289 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xa4, 0x92, 0xcf, 0x4a, 0xc3, 0x40,
+	0x10, 0xc6, 0xdd, 0x44, 0x03, 0x4e, 0x14, 0xca, 0x5a, 0x64, 0x0d, 0x2a, 0x21, 0xa0, 0xe4, 0xb4,
+	0x85, 0xe8, 0x41, 0x3c, 0x54, 0x7a, 0xa8, 0xe0, 0x35, 0x17, 0x8f, 0x92, 0xea, 0xa6, 0x84, 0xc4,
+	0x9d, 0x35, 0x7f, 0xc9, 0xfb, 0xf9, 0x3c, 0x3e, 0x83, 0x64, 0x93, 0xd2, 0x20, 0x82, 0x60, 0x6f,
+	0xf9, 0x66, 0xbe, 0x6f, 0xc8, 0x6f, 0x66, 0xe1, 0x24, 0x52, 0xea, 0xa5, 0xc1, 0x3c, 0x8d, 0x33,
+	0x6c, 0xb8, 0xca, 0xb1, 0xc4, 0x1b, 0xe7, 0x38, 0x4e, 0x44, 0xf6, 0x56, 0x6c, 0xe4, 0xd9, 0x1a,
+	0x71, 0x9d, 0x89, 0x99, 0x96, 0xab, 0x2a, 0x9e, 0x45, 0xb2, 0xed, 0x7b, 0xde, 0x27, 0x01, 0x58,
+	0x28, 0x15, 0x8a, 0x8f, 0x4a, 0x14, 0x25, 0xbd, 0x04, 0xab, 0x8f, 0x32, 0xe2, 0x9a, 0xbe, 0x1d,
+	0x58, 0xfc, 0xb1, 0x93, 0xe1, 0x50, 0xa5, 0x73, 0xb0, 0x85, 0xac, 0x93, 0x1c, 0xe5, 0xbb, 0x90,
+	0x25, 0x33, 0xb4, 0xe9, 0x9c, 0x6f, 0x27, 0xf0, 0xe5, 0xb6, 0xbd, 0x94, 0x65, 0xde, 0x86, 0xe3,
+	0x00, 0x3d, 0x05, 0x2b, 0x7a, 0x2d, 0x13, 0x94, 0xcc, 0x74, 0x89, 0x7f, 0x18, 0x0e, 0xca, 0x99,
+	0xc3, 0xe4, 0x67, 0x90, 0x4e, 0xc0, 0x4c, 0x45, 0xcb, 0x88, 0x36, 0x76, 0x9f, 0x74, 0x0a, 0x07,
+	0x75, 0x94, 0x55, 0x82, 0x19, 0xba, 0xd6, 0x8b, 0x7b, 0xe3, 0x8e, 0x78, 0x5f, 0x04, 0x6c, 0xfd,
+	0x13, 0x85, 0x42, 0x59, 0x88, 0x3f, 0x39, 0x1e, 0x7e, 0xe3, 0xb8, 0xe0, 0xa3, 0x11, 0xff, 0x03,
+	0xa1, 0xd7, 0x60, 0xe6, 0x51, 0xc3, 0xf6, 0x5d, 0xe2, 0xdb, 0xc1, 0x94, 0xf7, 0x8b, 0xe7, 0x9b,
+	0xc5, 0xf3, 0x85, 0x6c, 0xc3, 0xce, 0xb0, 0x2b, 0x70, 0x70, 0xab, 0x79, 0x9f, 0x87, 0xbb, 0xd3,
+	0x2b, 0xb0, 0x9e, 0x64, 0x8d, 0xa9, 0xa0, 0xf6, 0xe8, 0x18, 0xce, 0xd1, 0x98, 0xc8, 0xdb, 0x5b,
+	0x59, 0xfd, 0x83, 0xf8, 0x0e, 0x00, 0x00, 0xff, 0xff, 0x8c, 0x05, 0xb1, 0x39, 0x35, 0x02, 0x00,
+	0x00,
 }
